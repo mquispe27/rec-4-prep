6ddd80943ecc03f78a6f25c3c010e76495cee8be
Matthew Quispe <mquispe@mit.edu>
2024-09-26 04:23:54 -0400

    prep 4 done

--- server/concepts/authenticating.ts
+++ server/concepts/authenticating.ts
@@ -33,10 +33,11 @@ export default class AuthenticatingConcept {
   }
 
   async getUserById(_id: ObjectId) {
-    // TODO 1: implement this operation
-    //  - use this.users.readOne(..)
-    //  - don't include the password (we've provided a helper function you can use!)
-    throw new Error("Not implemented!");
+    const user = await this.users.readOne({ _id });
+    if (!user) {
+      throw new NotFoundError(`User not found!`);
+    }
+    return this.redactPassword(user);
   }
 
   async getUsers(username?: string) {
@@ -55,10 +56,10 @@ export default class AuthenticatingConcept {
   }
 
   async updateUsername(_id: ObjectId, username: string) {
-    // TODO 2: implement this operation
-    //  - use this.users.partialUpdateOne(..)
-    //  - maintain the invariant that usernames are unique (we've provided a helper function!)
-    throw new Error("Not implemented!");
+    await this.assertUserExists(_id);
+    await this.assertUsernameUnique(username);
+    await this.users.partialUpdateOne({ _id }, { username });
+    return { msg: "Username updated successfully!" };
   }
 
   async delete(_id: ObjectId) {
